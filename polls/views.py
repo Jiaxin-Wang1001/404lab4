c3566030f6d61b57e3c153cd5ac4ca4e19b6bf9d
Jiaxin Wang <jiaxin13@cs.ualberta.ca>
2021-10-01 14:20:23 -0600

    add file

---
+++ polls/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from dijango.http import HttpResponse
+
+# Create your views here.
+
+def index(requests):
+  return HttpResponse("Hello, world. You're at the pools index.")
